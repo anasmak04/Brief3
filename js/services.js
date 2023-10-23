2a88b9d8e1302a73aa107e216691aac55dd5c2af
anasmak04 <anasdev8@gmail.com>
2023-10-23 11:24:58 +0200

    this commit contains project with some libraries in js

--- js/services.js
+++ js/services.js
@@ -7,49 +7,82 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 /// modals
+// const buttons = [
+//   {
+//     id: "myBtn1",
+//     content: "Content for Button 1",
+//   },
+//   {
+//     id: "myBtn2",
+//     content: "Content for Button 2",
+//   },
+//   {
+//     id: "myBtn3",
+//     content: "Content for Button 1",
+//   },
+//   {
+//     id: "myBtn4",
+//     content: "Content for Button 2",
+//   },
+//   {
+//     id: "myBtn5",
+//     content: "Content for Button 1",
+//   },
+//   {
+//     id: "myBtn6",
+//     content: "Content for Button 2",
+//   },
+// ];
 
-const buttons = [
-  {
-    id: "myBtn1",
-    content: "Content for Button 1",
-  },
-  {
-    id: "myBtn2",
-    content: "Content for Button 2",
-  },
-  {
-    id: "myBtn3",
-    content: "Content for Button 1",
-  },
-  {
-    id: "myBtn4",
-    content: "Content for Button 2",
-  },
-  {
-    id: "myBtn5",
-    content: "Content for Button 1",
-  },
-  {
-    id: "myBtn6",
-    content: "Content for Button 2",
-  },
-];
+// buttons.forEach((button) => {
+//   const func = () => {
+//     const myPopup = new Popup({
+//       id: "my-popup",
+//       title: "My Popup",
+//       content: button.content,
+//       widthMultiplier: 0.7,
+//     });
+//     myPopup.show();
+//   };
 
-buttons.forEach((button) => {
-  const func = () => {
-    const myPopup = new Popup({
-      id: "my-popup",
-      title: "My Popup",
-      content: button.content,
-      widthMultiplier: 0.7,
-    });
-    myPopup.show();
-  };
+//   const btn = document.getElementById(button.id);
 
-  const btn = document.getElementById(button.id);
+//   btn.addEventListener("click", func);
+// });
 
-  btn.addEventListener("click", func);
-});
+
+
+  
+
+let modal = document.getElementById('simpleModal');
+let modalBtn = document.getElementById('modalBtn');
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+function openModal(){
+  modal.style.display = 'block';
+}
+
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function myFunction() {
   var dropdowns = document.querySelectorAll(".myDropdown");
