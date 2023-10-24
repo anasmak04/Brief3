22f8782e837f3bc9b41ffe6a73f767655538d23c
anasmak04 <anasdev8@gmail.com>
2023-10-24 16:02:35 +0200

    added darkmode in other pages

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const buttons = document.querySelectorAll("#myBtn");
 const progressCircle = document.querySelector(".autoplay-progress svg");
 const progressContent = document.querySelector(".autoplay-progress span");
 
-
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
@@ -11,125 +10,119 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-
-
-
 function myFunction() {
   var dropdowns = document.querySelectorAll(".myDropdown");
-  dropdowns.forEach(function(dropdown) {
+  dropdowns.forEach(function (dropdown) {
     dropdown.classList.toggle("show");
   });
 }
 
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.querySelectorAll(".myDropdown");
-    dropdowns.forEach(function(dropdown) {
-      if (dropdown.classList.contains('show')) {
-        dropdown.classList.remove('show');
+    dropdowns.forEach(function (dropdown) {
+      if (dropdown.classList.contains("show")) {
+        dropdown.classList.remove("show");
       }
     });
   }
 };
 
-let theme_toggler = document.querySelector('#theme_toggler');
+let theme_toggler = document.querySelector("#theme_toggler");
 
-theme_toggler.addEventListener('click', function(){ 
-    document.body.classList.toggle('dark_mode');
+theme_toggler.addEventListener("click", function () {
+  document.body.classList.toggle("dark_mode");
 
+  let iconElement = theme_toggler.querySelector("i");
+  let lockIcon = document.querySelectorAll("#lockIcon");
+  let uber = document.querySelectorAll("#uber");
+  let fb = document.querySelectorAll("#facebook");
+  let twitter = document.querySelectorAll("#twitter");
+  let dribble = document.querySelectorAll("#dribble");
+  let dribble1 = document.querySelectorAll("#dribble1");
+  let github = document.querySelectorAll("#github");
+  let verified = document.querySelectorAll("#verified");
 
-    let iconElement = theme_toggler.querySelector('i');
-    let lockIcon = document.querySelectorAll('#lockIcon');
-    let uber = document.querySelectorAll('#uber');
-    let fb = document.querySelectorAll('#facebook');
-    let twitter = document.querySelectorAll('#twitter');
-    let dribble = document.querySelectorAll('#dribble');
-    let github = document.querySelectorAll('#github');
+  if (document.body.classList.contains("dark_mode")) {
+    iconElement.classList.remove("fa-sun");
+    iconElement.classList.add("fa-star");
 
-    if (document.body.classList.contains('dark_mode')) {
+    lockIcon.forEach((item) => {
+      item.src = "/images/padlock.png";
+    });
 
-        iconElement.classList.remove('fa-sun');
-        iconElement.classList.add('fa-star');
+    uber.forEach((item) => {
+      item.src = "/images/uber1.png";
+    });
 
-        lockIcon.forEach((item) => {
-          item.src = '/images/padlock.png';
-        })
+    fb.forEach((item) => {
+      item.src = "/images/facebook-app-white.png";
+    });
 
-        uber.forEach((item) => {
-          item.src = '/images/uber1.png';
-        })
+    twitter.forEach((item) => {
+      item.src = "/images/twitter-white.png";
+    });
 
-        fb.forEach((item) => {
-          item.src = '/images/facebook-app-white.png';
-        })
+    dribble.forEach((item) => {
+      item.src = "/images/dribble-white.png";
+    });
 
-        twitter.forEach((item) => {
-          item.src = '/images/twitter-white.png';
-        })
+    dribble1.forEach((item) => {
+      item.src = "/images/dribble-v.png";
+    });
 
-        dribble.forEach((item) => {
-          item.src = '/images/dribble-white.png';
-        })
+    github.forEach((item) => {
+      item.src = "/images/github-white.png";
+    });
 
-        github.forEach((item) => {
-          item.src = '/images/github-white.png';
-        })
+    verified.forEach((item) => {
+      item.src = "/images/check-white.png";
+    });
+  } else {
+    iconElement.classList.remove("fa-star");
+    iconElement.classList.add("fa-sun");
+    lockIcon.forEach((item) => {
+      item.src = "/images/lock.svg";
+    });
 
+    uber.forEach((item) => {
+      item.src = "/images/uber.png";
+    });
 
-    } else {
+    fb.forEach((item) => {
+      item.src = "/images/Facebook.png";
+    });
+    twitter.forEach((item) => {
+      item.src = "/images/twitter.png";
+    });
 
-        iconElement.classList.remove('fa-star');
-        iconElement.classList.add('fa-sun');
-        lockIcon.forEach((item) => {
-          item.src = '/images/lock.svg';
-        })
+    dribble.forEach((item) => {
+      item.src = "/images/Dribble.png";
+    });
+    github.forEach((item) => {
+      item.src = "/images/Github.png";
+    });
 
-        uber.forEach((item) => {
-          item.src = '/images/uber.png';
-        })
+    verified.forEach((item) => {
+      item.src = "/images/virefied.png";
+    });
 
-        fb.forEach((item) => {
-          item.src = '/images/Facebook.png';
-        })
-        twitter.forEach((item) => {
-          item.src = '/images/twitter.png';
-        })
-
-        
-        dribble.forEach((item) => {
-          item.src = '/images/Dribble.png';
-        })
-        github.forEach((item) => {
-          item.src = '/images/Github.png';
-        })
-
-
-    }
+    dribble1.forEach((item) => {
+      item.src = "/images/image 15.png";
+    });
+  }
 });
-
-
-
-
 
 const hamburger = document.querySelector(".hamburger");
 const flexMenu = document.querySelector(".flex-menu");
 
-hamburger.addEventListener('click', function() {
-  flexMenu.classList.toggle('open');
-
+hamburger.addEventListener("click", function () {
+  flexMenu.classList.toggle("open");
 });
 
-const menuItems = document.querySelector('.flex-menu');
-menuItems.forEach(function(item) {
-  item.addEventListener('click', function() {
-    flexMenu.classList.remove('open');
+const menuItems = document.querySelector(".flex-menu");
+menuItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    flexMenu.classList.remove("open");
   });
 });
-
-
-
-
-
-
-
